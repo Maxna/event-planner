a27ea0486a38388ebf3595be00fd984cccd607d7
Student <example@example.com>
2018-05-25 09:59:29 -0700

    Add Event create and setPrice methods, successful test of newEvent

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -1,5 +1,20 @@
 package models;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Event {
+
+    private String price;
+    private ArrayList<String> coupons;
+
+    public Event() {
+        this.coupons = new ArrayList<String>();
+    }
+
+    public void setPrice(String total) {
+        this.price = total;
+    }
 
 }
--- src/test/java/models/EventTest.java
+++ src/test/java/models/EventTest.java
@@ -1,4 +1,15 @@
 package models;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
 public class EventTest {
+
+    @Test
+    public void newEvent_makesCorrectly_true() throws Exception {
+        Event testEvent = new Event();
+        testEvent.setPrice("100");
+        assertEquals(true, testEvent instanceof Event);
+    }
 }
