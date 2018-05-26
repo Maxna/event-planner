36b80269deaae51427d63b0037b114d4e30c3f65
Student <example@example.com>
2018-05-25 17:06:16 -0700

    Broke tests

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -1,11 +1,17 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Event {
 
     private int persons;
     private String meals;
+    private ArrayList<String> typesOfMeals;
     private String drinks;
+    private String typesOfDrinks;
     private String shows;
+    private String typesOfShows;
     private int price;
 
 
@@ -21,8 +27,23 @@ public class Event {
         return persons;
     }
 
+    public List typesOfMeals() {
+        ArrayList<String> foodItem = new ArrayList<String>() {{
+            add("steak");
+            add("seafood");
+        }};
+        return foodItem;
+    }
+
     public void setMeals(String food) {
-        this.meals = food;
+        this.meals = foodItem;
+        List<String> result = new ArrayList<String>();
+        if (this.meals.contains(steak)) {
+            result.add(steak);
+        } else if (this.meals.contains(seafood)) {
+            result.add(seafood);
+        }
+        return result;
     }
 
     public String getMeals() {
