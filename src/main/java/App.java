ce99ac1e92a2983e08bc62d73aa92b153a2fdd03
Student <example@example.com>
2018-05-30 20:39:45 -0700

    Followed along with car dealership lesson to build App and Getters

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,83 @@
+import models.Event;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.ArrayList;
+
 public class App {
+    public static void main(String[] args) {
+
+        Event event = new Event(10, "fish", "beer", "metallica", 100);
+
+        ArrayList<Event> allEvents = new ArrayList<Event>();
+        allEvents.add(event);
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        boolean programRunning = true;
+
+        while (programRunning) {
+            System.out.println("Welcome to Java Event Planning. Please select one of the following options: All Events, Search Price, Add Event, or Exit");
+
+            try {
+
+                String navigationChoice = bufferedReader.readLine();
+
+                if (navigationChoice.equals("All Events")) {
+                    for (Event individualEvent : allEvents) {
+                        System.out.println("----------------");
+                        System.out.println(individualEvent.getPerson());
+                        System.out.println(individualEvent.getMeal());
+                        System.out.println(individualEvent.getDrink());
+                        System.out.println(individualEvent.getShow());
+                        System.out.println(individualEvent.getPrice());
+                    }
+                } else if (navigationChoice.equals("Search Price")) {
+                    System.out.println("What is your total budget for this Event?");
+                    String stringUserMaxBudget = bufferedReader.readLine();
+                    int userMaxBudget = Integer.parseInt(stringUserMaxBudget);
+                    System.out.println("Great, here are some options in your price range:");
+                    for (Event individualEvent : allEvents) {
+                        if (individualEvent.budget(userMaxBudget)) {
+                            System.out.println("----------------");
+                            System.out.println(individualEvent.getPerson());
+                            System.out.println(individualEvent.getMeal());
+                            System.out.println(individualEvent.getDrink());
+                            System.out.println(individualEvent.getShow());
+                            System.out.println(individualEvent.getPrice());
+                        }
+                    }
+                } else if (navigationChoice.equals("Add Event")) {
+                    System.out.println("Great, Let's get started on your Event! First, how many people will be attending?");
+                    int userEventPerson = Integer.parseInt(bufferedReader.readLine());
+                    System.out.println("OK, and what type of food should there be?");
+                    String userEventMeal = bufferedReader.readLine();
+                    System.out.println("Awesome! What type of drinks should there be?");
+                    String userEventDrink = bufferedReader.readLine();
+                    System.out.println("And what type of entertainment should there be?");
+                    String userEventShow = bufferedReader.readLine();
+                    System.out.println("Finally, what will it cost?");
+                    int userEventPrice = Integer.parseInt(bufferedReader.readLine());
+                    Event userEvent = new Event(userEventPerson, userEventMeal, userEventDrink, userEventShow, userEventPrice);
+                    allEvents.add(userEvent);
+                    System.out.println("This Event sounds great!");
+                    System.out.println("----------------");
+                    System.out.println(userEvent.getPerson());
+                    System.out.println(userEvent.getMeal());
+                    System.out.println(userEvent.getDrink());
+                    System.out.println(userEvent.getShow());
+                    System.out.println(userEvent.getPrice());
+                } else if (navigationChoice.equals("Exit")){
+                    System.out.println("Goodbye!");
+                    programRunning = false;
+                } else {
+                    System.out.println("I'm sorry, we don't recognize your input");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -5,74 +5,144 @@ import java.util.List;
 
 public class Event {
 
-    private int persons;
-    private String meals;
-    private ArrayList<String> typesOfMeals;
-    private String drinks;
-    private String typesOfDrinks;
-    private String shows;
-    private String typesOfShows;
+    private int person;
+    private String meal;
+    private String drink;
+    private String show;
     private int price;
 
 
-    public Event() {
-
+    public boolean budget(int maxPrice){
+        return (price <= maxPrice);
     }
 
-    public void setPersons(int people) {
-        this.persons = people;
+    public Event(int person, String meal, String drink, String show, int price) {
+        this.person = person;
+        this.meal = meal;
+        this.drink = drink;
+        this.show = show;
+        this.price = price;
     }
 
-    public int getPersons() {
-        return persons;
+    public int getPerson() {
+        return person;
     }
 
-    public List typesOfMeals() {
-        ArrayList<String> foodItem = new ArrayList<String>() {{
-            add("steak");
-            add("seafood");
-        }};
-        return foodItem;
+    public String getMeal() {
+        return meal;
     }
 
-    public void setMeals(String food) {
-        this.meals = foodItem;
-        List<String> result = new ArrayList<String>();
-        if (this.meals.contains(steak)) {
-            result.add(steak);
-        } else if (this.meals.contains(seafood)) {
-            result.add(seafood);
-        }
-        return result;
+    public String getDrink() {
+        return drink;
     }
 
-    public String getMeals() {
-        return meals;
-    }
-
-    public void setDrinks(String beverage) {
-        this.drinks = beverage;
-    }
-
-    public String getDrinks() {
-        return drinks;
-    }
-
-    public void setShows(String music) {
-        this.shows = music;
-    }
-
-    public String getShows() {
-        return shows;
-    }
-
-    public void setPrice(int total) {
-        this.price = total;
+    public String getShow() {
+        return show;
     }
 
     public int getPrice() {
         return price;
     }
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    public void setPersons(int people) {
+//        this.persons = people;
+//    }
+//
+//    public int getPersons() {
+//        return persons;
+//    }
+//
+//    public List typesOfMeals() {
+//        ArrayList<String> foodItem = new ArrayList<String>() {{
+//            add("steak");
+//            add("seafood");
+//        }};
+//        return foodItem;
+//    }
+//
+//    public void setMeals(String food) {
+//        this.meals = foodItem;
+//        List<String> result = new ArrayList<String>();
+//        if (this.meals.contains(steak)) {
+//            result.add(steak);
+//        } else if (this.meals.contains(seafood)) {
+//            result.add(seafood);
+//        }
+//        return result;
+//    }
+//
+//    public String getMeals() {
+//        return meals;
+//    }
+//
+//    public void setDrinks(String beverage) {
+//        this.drinks = beverage;
+//    }
+//
+//    public String getDrinks() {
+//        return drinks;
+//    }
+//
+//    public void setShows(String music) {
+//        this.shows = music;
+//    }
+//
+//    public String getShows() {
+//        return shows;
+//    }
+//
+//    public void setPrice(int total) {
+//        this.price = total;
+//    }
+//
+//    public int getPrice() {
+//        return price;
+//    }
+
